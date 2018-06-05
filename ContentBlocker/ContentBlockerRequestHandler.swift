0a25f81a02d29e80939c0e0fe080c7b4b77be8ff
Leo Liu <leo.liu@aequilibrium.ca>
2018-06-04 19:48:39 -0700

    first commit

---
+++ ContentBlocker/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  ContentBlocker
+//
+//  Created by Leo Liu on 6/4/18.
+//  Copyright © 2018 hungryforcookies. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
